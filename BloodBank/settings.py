15b3721be55bc9fa910bdbcdb35d85f7f7bb5fdc
x22240471 <x22240471@student.ncirl.ie>
2023-12-15 03:23:45 +0000

    updated style

--- BloodBank/settings.py
+++ BloodBank/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'iw8)hzniv-@68f)p)4s6cxgwoi%j)m$y6!+sqah%ew(*^%t+wh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['x22240471-bloodbank.us-east-2.elasticbeanstalk.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
